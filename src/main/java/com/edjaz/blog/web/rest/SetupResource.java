ea67dcc7e64f4faa8cf7b907c3e88909a3e4efbd
Dimitri KAHN <dimitri@d-kahn.net>
2018-01-31 09:31:29 +0100

    wip

--- src/main/java/com/edjaz/blog/web/rest/SetupResource.java
+++ src/main/java/com/edjaz/blog/web/rest/SetupResource.java
@@ -3,16 +3,34 @@ package com.edjaz.blog.web.rest;
 import com.codahale.metrics.annotation.Timed;
 import com.edjaz.blog.domain.User;
 import com.edjaz.blog.repository.UserRepository;
+import com.edjaz.blog.security.AuthoritiesConstants;
+import com.edjaz.blog.security.jwt.JWTConfigurer;
+import com.edjaz.blog.security.jwt.TokenProvider;
 import com.edjaz.blog.service.UserService;
+import com.edjaz.blog.service.dto.UserDTO;
+import com.edjaz.blog.web.rest.errors.EmailAlreadyUsedException;
+import com.edjaz.blog.web.rest.errors.LoginAlreadyUsedException;
+import com.edjaz.blog.web.rest.util.HeaderUtil;
+import com.edjaz.blog.web.rest.vm.ManagedUserVM;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.github.jhipster.web.util.ResponseUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.Collections;
 import java.util.Optional;
 
 @RestController
@@ -25,9 +43,18 @@ public class SetupResource {
 
     private final UserService userService;
 
-    public SetupResource(UserRepository userRepository, UserService userService) {
+    private final UserDetailsService userDetailsService;
+
+    private final AuthenticationManager authenticationManager;
+
+    private final TokenProvider tokenProvider;
+
+    public SetupResource(UserRepository userRepository, UserService userService, UserDetailsService userDetailsService, AuthenticationManager authenticationManager, TokenProvider tokenProvider) {
         this.userRepository = userRepository;
         this.userService = userService;
+        this.userDetailsService = userDetailsService;
+        this.authenticationManager = authenticationManager;
+        this.tokenProvider = tokenProvider;
     }
 
 
@@ -51,4 +78,61 @@ public class SetupResource {
                 .getId()
         );
     }
+
+
+    @PutMapping("/setup/user")
+    @Timed
+    @PreAuthorize("isFirstSetup()")
+    public ResponseEntity<JWTToken> updateUser(@Valid @RequestBody ManagedUserVM userDTO) {
+        log.debug("REST request to update User : {}", userDTO);
+        Optional<User> existingUser = userRepository.findOneByEmailIgnoreCase(userDTO.getEmail());
+        if (existingUser.isPresent() && (!existingUser.get().getId().equals(userDTO.getId()))) {
+            throw new EmailAlreadyUsedException();
+        }
+        existingUser = userRepository.findOneByLogin(userDTO.getLogin().toLowerCase());
+        if (existingUser.isPresent() && (!existingUser.get().getId().equals(userDTO.getId()))) {
+            throw new LoginAlreadyUsedException();
+        }
+
+        //Met les droits admin par defaut
+        userDTO.setAuthorities(Collections.singleton(AuthoritiesConstants.ADMIN));
+
+        Optional<UserDTO> updatedUser = userService.updateUser(userDTO);
+        userService.changePassword(userDTO.getPassword());
+
+        // Log l'utilisateur
+        UsernamePasswordAuthenticationToken authenticationToken =
+            new UsernamePasswordAuthenticationToken(userDTO.getLogin(), userDTO.getPassword());
+
+        Authentication authentication = this.authenticationManager.authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = tokenProvider.createToken(authentication, false);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JWTConfigurer.AUTHORIZATION_HEADER, "Bearer " + jwt);
+        return new ResponseEntity<>(new JWTToken(jwt), httpHeaders, HttpStatus.OK);
+    }
+
+
+    /**
+     * Object to return as body in JWT Authentication.
+     */
+    static class JWTToken {
+
+        private String idToken;
+
+        JWTToken(String idToken) {
+            this.idToken = idToken;
+        }
+
+        @JsonProperty("id_token")
+        String getIdToken() {
+            return idToken;
+        }
+
+        void setIdToken(String idToken) {
+            this.idToken = idToken;
+        }
+    }
+
 }
--- src/test/java/com/edjaz/blog/web/rest/SetupResourceIntTest.java
+++ src/test/java/com/edjaz/blog/web/rest/SetupResourceIntTest.java
@@ -3,8 +3,11 @@ package com.edjaz.blog.web.rest;
 
 import com.edjaz.blog.BlogApp;
 import com.edjaz.blog.domain.Blog;
+import com.edjaz.blog.domain.User;
 import com.edjaz.blog.repository.BlogRepository;
+import com.edjaz.blog.repository.UserRepository;
 import com.edjaz.blog.repository.search.BlogSearchRepository;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,6 +15,7 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -22,8 +26,10 @@ import org.springframework.web.context.WebApplicationContext;
 import javax.persistence.EntityManager;
 
 import static com.edjaz.blog.web.rest.BlogResourceIntTest.createEntity;
+import static org.hamcrest.Matchers.hasItem;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -35,6 +41,9 @@ public class SetupResourceIntTest {
     private EntityManager em;
 
     private MockMvc restSetupMockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Autowired
     private BlogRepository blogRepository;
@@ -68,6 +77,38 @@ public class SetupResourceIntTest {
             .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE));
     }
 
+    private static final String DEFAULT_ADMIN_USER_LOGIN = "admin";
+    private static final String DEFAULT_LANGKEY = "en";
+    private static final String DEFAULT_IMAGEURL = "http://placehold.it/50x50";
+    private static final String DEFAULT_EMAIL = "johndoe@localhost";
+
+
+    public User createDefaultAdminUser() {
+        User user = new User();
+        user.setLogin(DEFAULT_ADMIN_USER_LOGIN);
+        user.setPassword(RandomStringUtils.random(60));
+        user.setActivated(true);
+        user.setEmail(RandomStringUtils.randomAlphabetic(5) + DEFAULT_EMAIL);
+        user.setFirstName("admin");
+        user.setLastName("admin");
+        user.setImageUrl(DEFAULT_IMAGEURL);
+        user.setLangKey(DEFAULT_LANGKEY);
+        return user;
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void should_return_id_admin_when_no_blog_and_has_anonymous() throws Exception {
+        Long id = userRepository.findOneByLogin("admin")
+            .orElseThrow(() -> new UsernameNotFoundException("User admin not found"))
+            .getId();
+        restSetupMockMvc.perform(get("/api/setup/user"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(content().string(id.toString()))
+        ;
+    }
+
 
     @Test
     @WithAnonymousUser
@@ -81,6 +122,32 @@ public class SetupResourceIntTest {
 
 
     @Test
+    @WithAnonymousUser
+    public void should_id_admin_return_forbiden_when_has_blog_has_anonymous() throws Exception {
+        blog = createEntity(em);
+        blogRepository.saveAndFlush(blog);
+        restSetupMockMvc.perform(get("/api/setup/user"))
+            .andExpect(status().isForbidden());
+    }
+
+
+    @Test
+    @WithMockUser(username="admin",roles={"USER","ADMIN"})
+    public void should_id_admin_return_true_when_has_blog_has_admin() throws Exception {
+        Long id = userRepository.findOneByLogin("admin")
+            .orElseThrow(() -> new UsernameNotFoundException("User admin not found"))
+            .getId();
+
+        blog = createEntity(em);
+        blogRepository.saveAndFlush(blog);
+        restSetupMockMvc.perform(get("/api/setup/user"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(content().string(id.toString()));
+    }
+
+
+    @Test
     @WithMockUser(username="admin",roles={"USER","ADMIN"})
     public void should_return_true_when_has_blog_has_admin() throws Exception {
         blog = createEntity(em);
@@ -90,13 +157,21 @@ public class SetupResourceIntTest {
             .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE));
     }
 
-
     @Test
     @WithMockUser(username="user",roles={"USER"})
     public void should_return_forbiden_when_has_blog_has_admin() throws Exception {
         blog = createEntity(em);
         blogRepository.saveAndFlush(blog);
         restSetupMockMvc.perform(get("/api/setup"))
+            .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(username="user",roles={"USER"})
+    public void should_id_admin_return_forbiden_when_has_blog_has_admin() throws Exception {
+        blog = createEntity(em);
+        blogRepository.saveAndFlush(blog);
+        restSetupMockMvc.perform(get("/api/setup/user"))
             .andExpect(status().isForbidden());
     }
 
